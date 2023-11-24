7f04829cc4dbef28b88a7ae5210ba3f1cbd885dc
eleilop <eloyleivalopez48@gmail.com>
2023-11-24 14:28:26 +0100

    CAP06 EJ04

---
+++ EJ04/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int i=1;i<=20;i++) {
+            int numero = (int)(Math.random()*11);
+            System.out.print(numero+" ");
+        }
+    }
+}
