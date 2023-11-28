46e31c5832224c979f965d28b3305f1f1accb9c6
eleilop <eloyleivalopez48@gmail.com>
2023-11-28 19:43:07 +0100

    CAP06 EJ06

---
+++ EJ06/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int secreto = (int)(Math.random()*101);
+        boolean es_correcto = false;
+        for (int i=4;i>=0 || es_correcto==true;i--){
+            System.out.print("Adivina el número: ");
+            int numero = Integer.parseInt(System.console().readLine());
+            if (numero==secreto) {
+                System.out.println("ENHORABUENA!!! Has acertado el número.");
+                break;
+            }
+            else 
+                System.out.printf("Número incorrecto.%nTe quedan %d intento/s.%n%s%n%n", i, (numero>secreto)?"El número es menor que "+numero:"El número es mayor que "+numero);
+        }
+    }
+}
