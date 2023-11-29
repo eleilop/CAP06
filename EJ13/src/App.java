21e28e0d4debcc6c27ebce804d64c74403054db0
eleilop <eloyleivalopez48@gmail.com>
2023-11-29 16:21:24 +0100

    CAP06 EJ13

--- EJ13/src/App.java
+++ EJ13/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        while (true) {
+            int dado1 = (int)(Math.random()*6+1);
+            int dado2 = (int)(Math.random()*6+1);
+            System.out.println(" _   _");
+            System.out.printf("|%d| |%d|%n", dado1, dado2);
+            System.out.println("¯¯¯ ¯¯¯");
+            if (dado1==dado2)
+                break;
+        }
     }
 }
